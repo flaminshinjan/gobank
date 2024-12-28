0eaa2c7b344098e9c714c32809a94fa1061788f5
Shinjan Patra <srikpatra.2008@gmail.com>
2024-12-28 23:53:29 +0530

    fix: indentation changed lol

--- api.go
+++ api.go
@@ -8,30 +8,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-
-func WriteJSON(w http.ResponseWriter, status int, v any) error {
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
-	return json.NewEncoder(w).Encode(v)
-}
-
-
-type apiFunc func(http.ResponseWriter, *http.Request) error
-
-type APIError struct {
-	Error string
-}
-
-
-func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request){
-		if err := f(w,r) ; err != nil{
-			WriteJSON(w, http.StatusBadRequest, APIError{Error: err.Error()} )
-		}
-	}
-	
-}
-
 type APIServer struct{
 	listenAddr string
 }
@@ -89,4 +65,27 @@ func(s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) erro
 
 func(s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error{
 	return nil
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type APIError struct {
+	Error string
+}
+
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		if err := f(w,r) ; err != nil{
+			WriteJSON(w, http.StatusBadRequest, APIError{Error: err.Error()} )
+		}
+	}
+	
 }
